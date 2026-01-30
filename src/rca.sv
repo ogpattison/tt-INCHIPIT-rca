433797569a5fa2425d700c5446d949faaa8e18f6
Lorenzo Barbero <mdfkxzexz@gmail.com>
2026-01-30 20:53:15 +0100

    Completed

--- src/rca.sv
+++ src/rca.sv
@@ -1,35 +1,34 @@
 //  Module: rca
-//    Completed
+//
 module rca
     (
-        input logic [2:0] a,
-        input logic [2:0] b,
-        input logic cin,
+        input  logic [2:0] a,
+        input  logic [2:0] b,
+        input  logic       cin,
         output logic [2:0] sum,
-        output logic cout
+        output logic       cout
     );
 
     // Internals
     logic [2:0] carry;
 
     genvar i;
-    generate;
-        for(i=0; i< /*Completare qui*/; i=i+1) begin: gen_rca 
-            if (i == 0) begin: gen_0
-                fa_module u(
-                    .a(a[i]),
-                    .b(b[i]),
-                    .cin(cin),
-                    .y(sum[i]),
+    generate
+        for (i = 0; i < 3; i = i + 1) begin : gen_rca
+            if (i == 0) begin : gen_0
+                fa_module u (
+                    .a   (a[i]),
+                    .b   (b[i]),
+                    .cin (cin),
+                    .y   (sum[i]),
                     .cout(carry[i])
                 );
-            end else begin: gen_gt0
-                /* Completare qui. */
-                fa_module u(
-                    .a(a[i]),
-                    .b(b[i]),
-                    .cin(carry[i-1]),
-                    .y(sum[i]),
+            end else begin : gen_gt0
+                fa_module u (
+                    .a   (a[i]),
+                    .b   (b[i]),
+                    .cin (carry[i-1]),
+                    .y   (sum[i]),
                     .cout(carry[i])
                 );
             end
@@ -38,4 +37,4 @@ module rca
 
     assign cout = carry[2];
 
-endmodule: rca
+endmodule : rca
